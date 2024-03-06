226d168927fb42788d22d1a92221417ba3e916dc
Jagdeep Kaur <200533351@student.georgianc.on.ca>
2024-03-06 11:10:56 -0500

    project done

---
+++ app/src/main/java/com/example/facebook/MainActivity2.kt
@@ -0,0 +1,47 @@
+package com.example.facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.facebook.databinding.ActivityMain2Binding
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    lateinit var quizModelList : MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+    }
+
+    private fun getDataFromFirebase(){
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    for (snapshot in dataSnapshot.children) {
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+
+
+    }
+}
